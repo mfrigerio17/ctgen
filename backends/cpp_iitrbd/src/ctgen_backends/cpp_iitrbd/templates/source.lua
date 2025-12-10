mfrigerio17/ctgen

commit f0b251f47e0b43b195bb27c37458c0ebcf808334

Generate one more constructor in the container, for parametric models [cpp]
To be able to pass an instance of the ModelParameters at creation
time.

backends/cpp_iitrbd/src/ctgen_backends/cpp_iitrbd/templates/source.lua
@@ -96,9 +96,20 @@ end
 local function container_class_methods(env)
     local template = [[
 «heading»
+@if ctModelMetadata.isParametric() then
+«qualifier»::«class»::«class»(const «ids.types.parameters_status»& initial) :
+«ctor_init_list», «meta.members.parameters»(initial)
+{}
+
+«qualifier»::«class»::«class»() :
+«ctor_init_list», «meta.members.parameters»(/*do we want the default instance of «ids.types.parameters_status» ?*/)
+{}
+@else
 «qualifier»::«class»::«class»() :
 «ctor_init_list»
 {}
+@end
+
 
 «heading»
 void «qualifier»::«class»::«meta.members.update»(const «ids.locals.variables_status_t»& «ids.locals.formalParams.varsStatus»)
@@ -110,6 +121,7 @@ void «qualifier»::«class»::«meta.members.update»(const «ids.locals.variab
 ]]
     local meta = env.ids.container_class
     local localenv = {
+        ctModelMetadata = env.ctModelMetadata,
         ctor_init_list = constructor_initializer_list(env.transforms, env.ids),
         class     = meta.class_name,
         meta      = meta,

backends/cpp_iitrbd/src/ctgen_backends/cpp_iitrbd/templates/header.lua
@@ -133,11 +133,14 @@ local container_class_definition_template = [[
 @local META = ids.container_class
 struct «META.class_name»
 {
-    «META.class_name»();
 @if ctModelMetadata.isParametric() then
+    «META.class_name»();
+    «META.class_name»(const «ids.types.parameters_status»& initial);
     void «META.members.update_params»(const «ids.types.parameters_status»& mp) {
         «META.members.parameters» = mp;
     }
+@else
+    «META.class_name»();
 @end
 
     void «META.members.update»(const «ids.locals.variables_status_t»&);

commit 897d7c84761be8a489b79830c24ca8e78f480808

Fix bug when generating templated code [cpp]
Introduced in f0b251f47e.
I forgot to have the cpp template heading in the constructor.

backends/cpp_iitrbd/src/ctgen_backends/cpp_iitrbd/templates/source.lua
@@ -101,6 +101,7 @@ local function container_class_methods(env)
 «ctor_init_list», «meta.members.parameters»(initial)
 {}
 
+«heading»
 «qualifier»::«class»::«class»() :
 «ctor_init_list», «meta.members.parameters»(/*do we want the default instance of «ids.types.parameters_status» ?*/)
 {}